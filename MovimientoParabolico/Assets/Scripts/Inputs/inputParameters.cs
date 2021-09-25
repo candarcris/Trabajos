abdf2e530aef139b11ca88f60a08ebe293eb2d1e
Cristian Arroyo <candarcristian@gmail.com>
2021-09-24 20:42:30 -0500

    Añadido proyecto movimiento parabólico

---
+++ MovimientoParabolico/Assets/Scripts/Inputs/inputParameters.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class inputParameters : MonoBehaviour
+{
+    public TiroParabolico tiroParabolico;
+    public InputField velInput;
+    public InputField angInput;
+    public InputField distEstimadaInput;
+    public Button iniciarSimulacion;
+    public Button reiniciar;
+    public GameObject msjErrorNegativos;
+    public GameObject msjErrorRangoAngulos;
+    public GameObject msjDatosIncompletos;
+    public Text textoRespuestaDistMax;
+    public Text felicitaciones;
+    public Text incorrecto;
+    public static float vel;
+    public static float ang;
+    public static float estimaciondist;
+    double radianes = 0;
+    double radianesPor2 = 0;
+    double pi = 3.1416;
+    public static double seno = 0;
+    public static double dosSeno = 0;
+    public static double coseno = 0;
+
+    void Awake()
+    {
+        velInput.characterLimit = 5;
+        angInput.characterLimit = 2;
+        distEstimadaInput.characterLimit = 10;
+        iniciarSimulacion.interactable = false;
+    }
+
+    void Update()
+    {
+        float.TryParse(velInput.text, out float resultvel);
+        vel = resultvel;
+
+        float.TryParse(angInput.text, out float resultang);
+        ang = resultang;
+
+        float.TryParse(distEstimadaInput.text, out float resultdistEstimada);
+        estimaciondist = resultdistEstimada;
+
+        radianes = ang * (pi/180);
+        radianesPor2 = (ang * 2) * (pi/180);
+
+        seno = Mathf.Sin(((float)radianes));
+        dosSeno = Mathf.Sin(((float)radianesPor2));
+        coseno = Mathf.Cos(((float)radianes));
+
+        if(velInput.text != "" && angInput.text != "" && distEstimadaInput.text != "")
+        {
+            iniciarSimulacion.interactable = true;
+        }
+        else
+        {
+            iniciarSimulacion.interactable = false;
+        }
+
+        ComprobarInputFields();
+    }
+
+    //Esta funcion comprueba los campos de velocidad y angulo iniciales, revisando siestan dentro de los rangos permitidos
+    public void ComprobarInputFields()
+    {
+        if(vel < 0)
+        {
+            msjErrorNegativos.SetActive(true);
+            velInput.text = "";
+            StartCoroutine (MsjError());
+        }
+
+        if(ang < 0 || ang > 90)
+        {
+            msjErrorRangoAngulos.SetActive(true);
+            angInput.text = "";
+            StartCoroutine (MsjAngulos());
+        }
+    }
+
+    //Esta funcion comprueba si todos los datos han sido diligenciados
+    public void ComprobarDatosDiligenciados()
+    {
+
+        if(velInput.text == "" || angInput.text == "" || distEstimadaInput.text == "")
+        {
+            msjDatosIncompletos.SetActive(true);
+            StartCoroutine (MsjDatosIncompletos());
+        }
+
+        else
+        {
+            this.gameObject.SetActive(false);
+            textoRespuestaDistMax.text = "";
+            felicitaciones.text = "";
+            incorrecto.text = "";
+        }
+    }
+
+    //Estas corrutinas nos ayudan a mejorar la usabilidad en los mensajes de error, dandoles un tiempo a los mensajes a ser leidos y borrar lo introducido para minimizar el error "typo" o de tecleo
+    IEnumerator MsjError()
+    {
+        velInput.interactable = false;
+        yield return new WaitForSeconds(1.4f);
+        msjErrorNegativos.SetActive(false);
+        velInput.interactable = true;
+    }
+
+    IEnumerator MsjAngulos()
+    {
+        angInput.interactable = false;
+        yield return new WaitForSeconds(1.4f);
+        msjErrorRangoAngulos.SetActive(false);
+        angInput.interactable = true;
+    }
+
+    IEnumerator MsjDatosIncompletos()
+    {
+        iniciarSimulacion.interactable = false;
+        yield return new WaitForSeconds(1.4f);
+        msjDatosIncompletos.SetActive(false);
+        iniciarSimulacion.interactable = true;
+    }
+
+    public void Reiniciar()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ MovimientoParabolico/Assets/Scripts/TiroParabolico.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TiroParabolico : MonoBehaviour
+{    
+    public inputParameters inputParameters;
+    public GameObject circulo;
+    public Transform objetivo;
+    float alturaMax;
+    public static float distMax;
+    float velFinal;
+
+    public float gravity = 9.8f;
+    
+    void Update()
+    {
+        
+    }
+
+    public void Lanzamiento()
+    {
+        alturaMax = ((Mathf.Pow(inputParameters.vel, 2)) * (Mathf.Pow(((float)inputParameters.seno), 2))) / (2 * 9.8f);
+        distMax = ((Mathf.Pow(inputParameters.vel, 2)) * (((float)inputParameters.dosSeno))) / 9.8f;
+        velFinal = (inputParameters.vel * ((float)inputParameters.coseno)) + (inputParameters.vel * ((float)inputParameters.seno));
+
+        float tolerancia = (distMax * 5f) / 100;
+        float toleranciaMayor = distMax + tolerancia;
+        float toleranciaMenor = distMax - tolerancia;
+
+        objetivo.transform.position = new Vector2(distMax, 0);
+        Debug.Log(distMax);
+        Debug.Log(alturaMax);
+
+        inputParameters.textoRespuestaDistMax.text = "La distancia recorrida en x fue:" + distMax;
+
+
+        if(inputParameters.estimaciondist == distMax || inputParameters.estimaciondist <= toleranciaMayor && inputParameters.estimaciondist >= toleranciaMenor)
+        {
+            inputParameters.felicitaciones.text = "Felicitaciones, tu respuesta es correcta";
+        }
+
+        else
+        {
+            inputParameters.incorrecto.text = "No has acertado, intentalo de nuevo";
+        }
+        
+
+        //------------------------------------------------------------------------------------------------------------------------
+        //Esta prueba se realizó para confirmar la veracidad de los datos
+        //float prueba = (Mathf.Pow(inputParameters.vel, 2)) * (Mathf.Pow(((float)inputParameters.seno), 2));
+        //float prueba2 = 2 * gravity;
+        //Debug.Log(prueba / prueba2);
+
+        //Debug.Log(inputParameters.coseno);
+        //-------------------------------------------------------------------------------------------------------------------------
+
+        
+        Rigidbody2D circuloRigidBody = circulo.GetComponent<Rigidbody2D>();
+        Physics2D.gravity = Vector2.up * -9.8f;
+        circuloRigidBody.gravityScale = 1;
+        circuloRigidBody.velocity = new Vector2((inputParameters.vel * ((float)inputParameters.coseno)), (inputParameters.vel * ((float)inputParameters.seno)));
+    }
+
+}
